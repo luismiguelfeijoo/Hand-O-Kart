9f76a49c9e6f0507479607a583d4efe85a923431
Luismi Feijoo <luism.feijoo26@gmail.com>
2019-12-14 12:09:27 +0100

    Fix the draw() of obstacle to better represent the animation

--- js/obstacle.js
+++ js/obstacle.js
@@ -5,8 +5,8 @@ class Obstacle {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
 
-    this.width = 30;
-    this.height = 30;
+    this.width = 0;
+    this.height = 0;
 
     this.sx = 395.5;
     this.sy = 1.5;
@@ -17,17 +17,19 @@ class Obstacle {
     this.posY = this.gameHeight - 200;
 
     this.vy = 5;
-    this.vz = 10;
+    this.vz = 5;
 
     this.image = new Image();
     this.image.src = "./img/Mario Kart.png"
   }
 
   draw() {
+    this.ctx.save()
     this.ctx.beginPath();
-    this.ctx.moveTo(this.posX + 7.5, this.posY + 7.5)
-    this.ctx.drawImage(this.image, this.sx, this.sy, this.sWidth, this.sHeight, this.posX - 7.5, this.posY - 7.5, this.width, this.height);
-    this.ctx.closePath()
+    this.ctx.translate(this.posX - this.width , this.posY)
+    this.ctx.drawImage(this.image, this.sx, this.sy, this.sWidth, this.sHeight, this.width/2, this.height/2, this.width, this.height);
+    this.ctx.closePath();
+    this.ctx.restore();
   }
 
   move() {
